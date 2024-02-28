e3ca77f845535a7a2872d3626695bcdf06d59ef9
Joujou Tik <christopher.guemi@laposte.net>
2024-02-28 09:49:59 +0100

    1

---
+++ recipe-page/src/App.jsx
@@ -0,0 +1,35 @@
+function App() {
+  return (
+    <>
+    <main>
+      <div className="feuille">
+        <div className="ensemble">
+        <img src="/image-omelette.jpeg" alt="image d'omelette"></img>
+        <h1>Simple omelette recipe</h1>
+        <p>Change the appearance of Atlas. These settings control experimental behavior of Atlas and should be used with discretion. Dark mode is currently unavailable for Charts.</p>
+        <div className="red">
+          <h3>Préparation time</h3>
+          <ul>
+            <li>total</li>
+            <li>Préparation</li>
+            <li>Cooking</li>
+          </ul>
+        </div>
+        <div className="liste">
+          <h2>Ingrédients</h2>
+          <ul>
+            <li>2-3 truc</li>
+            <li>legerreltn</li>
+            <li>abolo</li>
+            <li>bouloufkou</li>
+            <li>caramctr</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    </main>
+    </>
+  )
+}
+
+export default App
